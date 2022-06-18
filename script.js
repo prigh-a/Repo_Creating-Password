e461fd0b02368a67e68e5322c1fc68b1e8f1bd00
prigh-a <piya.gle015@gmail.com>
2022-06-18 14:09:51 -0700

    created password generator

---
+++ script.js
@@ -0,0 +1,18 @@
+function generate() {
+    let password = "";
+    let length = "8max";
+
+}
+
+generate();
+function generateRandompassword() {
+    return String.fromCharCodegenerator
+}
+
+function generateRandomSymbol() {
+    const symbols = "~!@#$%^&*?'-=/,.{}()[]<>";
+    return symbols[generateRandompassword(0, symbols.length = 1)]
+
+    document.getElementById("result").textContent = password;
+    
+}
